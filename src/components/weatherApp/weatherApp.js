f1f4a5d62743656c44d79599e982e917e8b591b6
Juan Villamoros <jcvillamorosg@unipacifico.edu.co>
2022-05-19 23:50:37 -0500

    consumo de api externa para conocer el clima de cualquier ciudad del mundo, en tiempo real. Usando el hook de react useState y useEffect .

---
+++ src/components/weatherApp/weatherApp.js
@@ -0,0 +1,50 @@
+import { useState, useEffect } from "react";
+import Loading from "../loading/loading";
+import WeatherForm from "../weatherForm/WeatherForm";
+import WeatherMainInfo from "../weatherMainInfo/weatherMainInfo"
+
+import styles from "./weatherApp.module.css";
+
+export default function WeatherApp() {
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    loadInfo();
+  }, []);
+
+  useEffect(() => {
+    document.title = "Weather | " + weather?.location?.name ?? "";
+  }, [weather]);
+
+  async function loadInfo(city = "buenaventura") {
+    console.log(
+      `${process.env.REACT_APP_URL}&key=${process.env.REACT_APP_KEY}&q=${city}`
+    );
+    try {
+      const request = await fetch(
+        `${process.env.REACT_APP_URL}&key=${process.env.REACT_APP_KEY}&q=${city}`
+      );
+      const json = await request.json();
+      console.log(json);
+
+      setTimeout(() => {
+        setWeather({ ...json });
+      }, 2000);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  function handleOnChangeCity(city) {
+    setWeather(null);
+    loadInfo(city);
+  }
+
+  return (
+    <div className={styles.weatherContainer}>
+      <WeatherForm onChangeCity={handleOnChangeCity} />
+      {weather ? <WeatherMainInfo weather={weather} /> : <Loading />}
+    </div>
+  );
+}
+
